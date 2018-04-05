2a0a011081d6d04e7095214eaf364ac839b80e62
Hannes Nel <hannesne@users.noreply.github.com>
2018-04-05 14:41:00 +1200

    Figured out how to connect to the named pipe from the console app. Need to use a different name and then it works fine. Extended the app to be able to send messages bidirectionally between the two processes.

--- UWPApp/MainPage.xaml.cs
+++ UWPApp/MainPage.xaml.cs
@@ -4,11 +4,15 @@ using System.Diagnostics;
 using System.IO;
 using System.IO.Pipes;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Runtime.InteropServices.WindowsRuntime;
 using System.Security.Principal;
 using System.Threading.Tasks;
+using Windows.ApplicationModel;
+using Windows.ApplicationModel.Store;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI.Core;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -16,6 +20,7 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using Microsoft.Win32.SafeHandles;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -26,8 +31,10 @@ namespace UWPApp
     /// </summary>
     public sealed partial class MainPage : Page
     {
-        private StreamReader reader;
-        private StreamWriter writer;
+        private static StreamReader reader;
+        private static StreamWriter writer;
+        private DateTime lastSentMessage;
+        private bool waitingForReply;
 
         public MainPage()
         {
@@ -43,54 +50,67 @@ namespace UWPApp
 
 
 
-        private static void PipeServerThread()
+        private void PipeServerThread()
         {
-            NamedPipeServerStream serverStream = new NamedPipeServerStream(@"LOCAL\mypipe", PipeDirection.InOut, 1, PipeTransmissionMode.Byte, PipeOptions.Asynchronous);
+            NamedPipeServerStream serverStream = new NamedPipeServerStream(@"LOCAL\mypipe", PipeDirection.InOut, 1, PipeTransmissionMode.Message, PipeOptions.Asynchronous);
             Debug.WriteLine("Waiting for connection");
+            
             serverStream.WaitForConnection();
             Debug.WriteLine("Connection established");
 
+            reader = new StreamReader(serverStream);
+            writer = new StreamWriter(serverStream);
+
+            while (true)
+            {
+                var responseMessage = reader.ReadLine();
+
+                string appendix;
+                if (waitingForReply)
+                {
+                    waitingForReply = false;
+                    TimeSpan duration = DateTime.Now - lastSentMessage;
+                    appendix = $" rountrip duration: {duration.TotalMilliseconds}ms";
+                }
+                else
+                {
+                    appendix = string.Empty;
+                }
+
+                Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () => ReceivedMessages.Text +=
+                    $"Received '{responseMessage}' from pipe {appendix} {Environment.NewLine}").AsTask().Wait();
+
+
+               }
 
         }
 
         private void SendMessage_OnClick(object sender, RoutedEventArgs e)
         {
-            DateTime pipeRequest = DateTime.Now;
+            if (writer == null)
+            {
+                ReceivedMessages.Text = "Pipe not open?";
+                return;
+            }
+            lastSentMessage = DateTime.Now;
+            waitingForReply = true;
             writer.WriteLine(Message.Text);
             writer.Flush();
-            reader.ReadLine();
-            DateTime pipeResponse = DateTime.Now;
-            TimeSpan pipeDuration = pipeResponse - pipeRequest;
-            ReceivedMessages.Text +=
-                $"Received response from pipe: {pipeResponse} after {pipeDuration.TotalMilliseconds}ms";
+            
         }
 
         private async void LaunchConsoleApp_OnClick(object sender, RoutedEventArgs e)
         {
-            await Windows.ApplicationModel.FullTrustProcessLauncher.LaunchFullTrustProcessForCurrentAppAsync();
+            await FullTrustProcessLauncher.LaunchFullTrustProcessForCurrentAppAsync();
             
-            //Task.Run((Action)PipeClientThread);
         }
 
-        private static void PipeClientThread()
+        public static Guid ToGuid(int value)
         {
-            var client = new NamedPipeClientStream(".", @"LOCAL\mypipe", PipeDirection.InOut, PipeOptions.Asynchronous);
-
-            client.Connect(5000);
-
-            Console.WriteLine("Connection established");
-
-            StreamReader reader = new StreamReader(client);
-            StreamWriter writer = new StreamWriter(client);
-            while (true)
-            {
-                var line = reader.ReadLine();
-                Console.WriteLine($"Message: {line}");
-
-                writer.WriteLine(String.Join("", line.Reverse()));
-                writer.Flush();
-            }
-
+            byte[] bytes = new byte[16];
+            BitConverter.GetBytes(value).CopyTo(bytes, 0);
+            return new Guid(bytes);
         }
+
     }
 }
